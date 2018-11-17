e3ef2278a14a0e6118cb2675044fd5ff480a0920
anupritaisno1 <www.anuprita804@gmail.com>
2018-11-17 04:04:38 +0000

    add wireguard snapshot
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

---
+++ net/wireguard/tools/show.d
@@ -0,0 +1,14 @@
+show.o: show.c containers.h ../uapi/wireguard.h ipc.h terminal.h \
+ encoding.h subcommands.h
+
+containers.h:
+
+../uapi/wireguard.h:
+
+ipc.h:
+
+terminal.h:
+
+encoding.h:
+
+subcommands.h:
